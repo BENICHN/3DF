b53f3b361701ef999c9cb06e96aa5a4097a739c2
Nathan Benichou <nathan.benichou31280@gmail.com>
2021-02-20 20:33:38 +0100

    unsafeupd

--- 3DFH/src/PixArray.hs
+++ 3DFH/src/PixArray.hs
@@ -4,7 +4,7 @@ import Colour
 import Data.List
 import Data.Ratio
 import qualified Data.Vector.Generic as G
-import Data.Vector.Storable (Vector, (//))
+import Data.Vector.Storable (Vector)
 import Utils
 
 data PixelArray = PixelArray Int Int (Vector Colour)
@@ -30,4 +30,4 @@ drawLine c ps@(xs, ys) pe@(xe, ye) arr@(PixelArray w h bits)
 
         steps = map (\(x1, x2) -> swapIf inv (x1, floor $ fromRatio x2)) . iterate (\(x1, x2) -> (x1 + dd1, x2 + dd2)) $ (s1, s2 % 1)
         entries = filter (\i -> 0 < i && i < G.length bits) . genericTake (abs d1) . map (\(x, y) -> w * fromIntegral y + fromIntegral x) $ steps
-     in PixelArray w h $ bits // zip entries (repeat c)
+     in PixelArray w h $ bits `G.unsafeUpd` zip entries (repeat c)
